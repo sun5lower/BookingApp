014fd3634b5bd95df02be140207ac0547886563f
Dagmara <dlegrange3@gmail.com>
2021-09-09 00:39:20 +0100

    Added search field in displayBookings.fxml & DisplayBookingsController

--- src/main/java/controllers/fxml/DisplayBookingsController.java
+++ src/main/java/controllers/fxml/DisplayBookingsController.java
@@ -1,4 +1,15 @@
 package controllers.fxml;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+
 public class DisplayBookingsController {
+
+    @FXML
+    TextField viewingSearchField;
+
+    public void search() {
+
+    }
 }
